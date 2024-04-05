dc61cf343fa0d8db6511c97b175589b4262124c4
Tega <beniyoke@gmail.com>
2024-04-05 22:59:50 +0100

    Creat Books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,13 @@ class BooksController < ApplicationController
   def index
   end
 
-  def edit
+  def create
+    puts book_params
+    render json: {:success=> true}
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:name)
   end
 end
