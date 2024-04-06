b5b9d70ed5ff2b03485c69b1d1cbee19a21ba0c7
Tega <beniyoke@gmail.com>
2024-04-06 05:59:30 +0100

    Unit test the books controller

--- Gemfile
+++ Gemfile
@@ -11,8 +11,6 @@ gem "sqlite3", "~> 1.4"
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
 
-gem 'rspec-rails', '~> 6.1', '>= 6.1.2'
-
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
 
@@ -40,6 +38,8 @@ gem "bootsnap", require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
+  gem 'rspec-rails', '~> 6.1', '>= 6.1.2'
+  gem "factory_bot_rails"
 end
 
 group :development do
--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,10 +1,45 @@
 require "rails_helper"
 
 describe "Books API", type: :request do
-  it "should return all books" do
-    get "api/v1/books"
-    expect(response).to have_http_status(:success)
+  endpoint = "/api/v1/books"
 
+  describe :get_books do
+    FactoryBot.create(:book, author: "Tega", title: "You")
+    FactoryBot.create(:book, author: "Terader", title: "Him")
+
+    it "should return all books" do
+      get endpoint
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body).size).to eq(2)
+    end
+
+  end
+
+  describe "POST /books" do
+
+    it "should create a new book" do
+      expect {
+        post endpoint, params: {book: {author: "A test author", title: "My title"}};
+      }.to change {Book.count}.from(0).to(1)
+
+      expect(response).to have_http_status(:created)
+    end
+
+  end
+
+  describe "DELETE /book " do
+    book = FactoryBot.create(:book, author: "Tega", title: "You")
+
+    it "should delete a book successfully" do
+    #   Create a book using Factory_bot first
+
+    expect {
+      delete "#{endpoint}/#{book.id}"
+    }.to change {Book.count}.from(1).to(0)
+
+    expect(response).to have_http_status(:no_content)
+
+    end
   end
 
 end
