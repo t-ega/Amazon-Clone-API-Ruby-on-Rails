748bd60b26df1fea95b8f79cbc0ea64c669e5387
Tega <beniyoke@gmail.com>
2024-04-08 15:36:55 +0100

    Create users endpoint

---
+++ app/controllers/api/v1/user_controller.rb
@@ -0,0 +1,6 @@
+class UserController < ApplicationController
+
+  def create
+
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,3 @@
+class User < ApplicationRecord
+  validates :password, presence: true, length: {minimum: 6}
+end
